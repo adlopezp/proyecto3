34d7e92ee213e9dc6b80f285c2c2fcaefdbeb620
dvergel <micosil001@hotmail.com>
2015-11-20 17:12:15 -0500

    commit final trazas WS

--- Centralizador/centralizador-ejb/src/main/java/co/edu/uniandes/ecos/statusquo/centralizador/ws/handlers/LogingServerHandler.java
+++ Centralizador/centralizador-ejb/src/main/java/co/edu/uniandes/ecos/statusquo/centralizador/ws/handlers/LogingServerHandler.java
@@ -9,19 +9,19 @@ import co.edu.uniandes.ecos.statusquo.centralizador.percistence.facade.Auditoria
 import co.edu.uniandes.ecos.statusquo.centralizador.persistence.entity.AuditoriaWs;
 import java.io.StringWriter;
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -30,6 +30,7 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -43,8 +44,10 @@ public class LogingServerHandler implements SOAPHandler<SOAPMessageContext> {
 
     @Override
     public boolean handleMessage(SOAPMessageContext mc) {
-       try {
+        try {
             final SOAPMessage message = mc.getMessage();
+            SOAPHeader header = message.getSOAPHeader();
+            NodeList userIdNode = header.getElementsByTagNameNS("*", "idtxcliente");
             Map<String, List<String>> headers;
             AuditoriaWs transaccion = new AuditoriaWs();
             List<String> variables = new ArrayList<String>();
@@ -55,34 +58,59 @@ public class LogingServerHandler implements SOAPHandler<SOAPMessageContext> {
             boolean direction = ((Boolean) mc.get(SOAPMessageContext.MESSAGE_OUTBOUND_PROPERTY));
             if (direction) {
                 headers = (Map<String, List<String>>) mc.get(SOAPMessageContext.HTTP_REQUEST_HEADERS);
-                if (headers!=null && headers.get("idtx") != null && !headers.isEmpty()) {
+                if (headers != null && headers.get("idtx") != null && !headers.isEmpty()) {
                     transaccion = auditoria.find(new BigDecimal(headers.get("idtx").get(0)));
                 }
                 transaccion.setFechaOut(new Date());
                 transaccion.setMensajeOut(sw.toString());
             } else {
-                HttpServletRequest req = (HttpServletRequest) mc.get(SOAPMessageContext.SERVLET_REQUEST);
-                transaccion.setHostActor(req.getRemoteAddr());
                 headers = (Map<String, List<String>>) mc.get(SOAPMessageContext.HTTP_REQUEST_HEADERS);
-                if (headers!=null && headers.get("idtx") != null && !headers.isEmpty()) {
+                if (headers != null && headers.get("idtx") != null && !headers.isEmpty()) {
                     transaccion = auditoria.find(new BigDecimal(headers.get("idtx").get(0)));
+                } else {
+                    HttpServletRequest req = (HttpServletRequest) mc.get(SOAPMessageContext.SERVLET_REQUEST);
+                    if (req != null) {
+                        transaccion.setHostActor(req.getRemoteAddr());
+                    }
                 }
                 transaccion.setFechaIn(new Date());
                 transaccion.setMensajeIn(sw.toString());
             }
-            if (headers==null || headers.get("idtx") == null || headers.isEmpty()) {
-                if (headers!=null && headers.get("idtxcliente") != null) {
+            if (headers == null) {
+                headers = new HashMap<String, List<String>>();
+            }
+            if (transaccion.getId() == null) {
+                if (headers != null && headers.get("idtxcliente") != null) {
                     transaccion.setIdcliente(headers.get("idtxcliente").get(0));
+                }
+
+                if (userIdNode != null && userIdNode.getLength() != 0) {
+                    transaccion.setIdcliente(userIdNode.item(0).getChildNodes().item(0).getNodeValue());
                 }
                 transaccion.setInOut(!direction);
                 auditoria.create(transaccion);
-                variables.clear();
-                variables.add(transaccion.getId().toString());
-                headers.put("idtx", variables);
+                if (!direction) {
+                    variables.clear();
+                    variables.add(transaccion.getId().toString());
+                    headers.put("idtx", variables);
+                } else {
+                    QName idtx = new QName("https://co.edu.uniandes.ecos.statusquo.operador.ws.handlers/", "idtxcliente");
+                    SOAPHeaderElement tag = header.addHeaderElement(idtx);
+                    tag.addTextNode(transaccion.getId().toString());
+                    message.saveChanges();
+                }
             } else {
-                variables.clear();
-                variables.add(transaccion.getId().toString());
-                headers.put("idtxcliente", variables);
+                userIdNode = header.getElementsByTagNameNS("*", "idtx");
+                if (userIdNode != null && userIdNode.getLength() != 0) {
+                    transaccion.setIdcliente(userIdNode.item(0).getChildNodes().item(0).getNodeValue());
+                }
+                QName idtx = new QName("https://co.edu.uniandes.ecos.statusquo.operador.ws.handlers/", "idtx");
+                SOAPHeaderElement tag = header.addHeaderElement(idtx);
+                tag.addTextNode(transaccion.getId().toString());
+                QName idtxcliente = new QName("https://co.edu.uniandes.ecos.statusquo.operador.ws.handlers/", "idtxcliente");
+                SOAPHeaderElement tag2 = header.addHeaderElement(idtxcliente);
+                tag2.addTextNode(transaccion.getIdcliente());
+                message.saveChanges();
                 auditoria.edit(transaccion);
             }
             return true;
