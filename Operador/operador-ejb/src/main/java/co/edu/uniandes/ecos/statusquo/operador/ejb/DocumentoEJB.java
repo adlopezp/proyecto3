f7c102cb91fb7b0da8aa0845306b12e845e8cdbb
Zamir García <zgarcia83@gmail.com>
2015-11-25 20:33:34 -0500

    Adición de restaurar (Parcialmente implementado)

--- Operador/operador-ejb/src/main/java/co/edu/uniandes/ecos/statusquo/operador/ejb/DocumentoEJB.java
+++ Operador/operador-ejb/src/main/java/co/edu/uniandes/ecos/statusquo/operador/ejb/DocumentoEJB.java
@@ -137,8 +137,14 @@ public class DocumentoEJB {
      */
     public void addFiles(Carpeta carpeta, List<Archivo> archivos, long state) {
         for (Archivo archivo : carpeta.getArchivos()) {
-            if (archivo.getEstado().getId() == state) {
-                archivos.add(archivo);
+            
+            if (archivo.getEstado().getId() == state ) {
+                System.out.print("Add file: " + archivo.getNombre());
+                if (!archivos.contains(archivo)) {
+                    System.out.println(" -> no está");
+                    archivos.add(archivo);
+                }
+                
             }
             if (carpeta.getCarpetasHijas() != null && !carpeta.getCarpetasHijas().isEmpty()) {
                 archivos.addAll(traerArchivosPapelera(carpeta.getCarpetasHijas()));
